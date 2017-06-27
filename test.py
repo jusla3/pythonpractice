52b524b3d8b1ba466346811e7d6d5375de2517a9
Lazaro Dos Santos <jusla3@yahoo.co.uk>
2017-06-28 00:01:21 +0100

    ways of printing hello world

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print 'Hello, world!'
+#printing a line
+# You can use  print..
+
+print 'Hello, world 1!'
+print('Hello, world 2!')
+
+
+# Or output to stdout:
+
+import sys
+
+sys.stdout.write('Hello  world 3!\n')
